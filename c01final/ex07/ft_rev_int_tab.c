a512a0f20fce4164198acdb2b84518e3b7c2027f
Lisa Jongmans <ljongman@e-r6-p7.s19.be>
2019-07-10 22:59:07 +0200

    no mains

---
+++ c01final/ex07/ft_rev_int_tab.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/09 22:06:33 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/10 16:21:17 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int index;
+	int c;
+
+	index = 0;
+	c = 0;
+	while (index < (size / 2))
+	{
+		c = tab[index];
+		tab[index] = tab[size - 1 - index];
+		tab[size - 1 - index] = c;
+		index++;
+	}
+}
---
+++ c01final/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/10 13:10:24 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/10 15:47:54 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int index;
+	int n;
+	int c;
+
+	index = 0;
+	c = 0;
+	while (index < size)
+	{
+		n = index + 1;
+		while (n < size)
+		{
+			if (tab[index] > tab[n])
+			{
+				c = tab[n];
+				tab[n] = tab[index];
+				tab[index] = c;
+			}
+			n++;
+		}
+		index++;
+	}
+}
